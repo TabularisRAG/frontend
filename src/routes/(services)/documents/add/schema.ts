02f5f5a853131586a03bceceb45ed0c47fb209a5
Akito <web@stoffelk.de>
2025-07-23 12:05:43 +0200

    added year validation

--- src/routes/(services)/documents/add/schema.ts
+++ src/routes/(services)/documents/add/schema.ts
@@ -6,7 +6,7 @@ export const MEGABYTE = 1024 * KILOBYTE;
 export let schema = z.object({
     file: z.array(z.file(m.doc_validation_file_missing()).max(MEGABYTE * 10, m.doc_validation_file_too_large())),
     title: z.string(m.doc_validation_title_missing()).min(3, m.doc_validation_title_too_short()),
-    year: z.int(m.doc_validation_year_missing()),
+    year: z.int(m.doc_validation_year_missing()).max(new Date().getFullYear(), m.doc_validation_year_too_high()),
     author: z.string().nonempty(m.doc_validation_author_missing()),
     keywords: z.string().array().min(1, m.doc_validation_keywords_missing())
 })  
