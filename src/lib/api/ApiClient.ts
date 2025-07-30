7eea213e959c730a9b39b65186e3329b87bb5f2c
Johannes Thiede <j.thiede@sap.com>
2025-07-30 23:14:23 +0200

    read api url fron env

--- src/lib/api/ApiClient.ts
+++ src/lib/api/ApiClient.ts
@@ -1,3 +1,3 @@
 export default abstract class APIClient {
-    protected serverURL = import.meta.env.API_URL || "http://localhost:8000"
+    protected serverURL = import.meta.env.VITE_API_URL || "http://localhost:8000"
 }
