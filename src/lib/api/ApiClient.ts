df660c5b5e76f51f4381f847aa2b10cc91ff22d3
Johannes Thiede <j.thiede@sap.com>
2025-07-31 11:25:24 +0200

    Update ApiClient.ts

--- src/lib/api/ApiClient.ts
+++ src/lib/api/ApiClient.ts
@@ -1,13 +1,3 @@
 export default abstract class APIClient {
-
-    public serverURL = this.getServerURL()
-  
-    private getServerURL(): string {
-        // For server-side (environment in Docker)
-        if (typeof window === 'undefined') {
-            return process.env.VITE_API_URL || 'http://localhost:8000'
-        }
-        // For client-side (browser)
-        return import.meta.env.VITE_API_URL || 'http://localhost:8000'
-    }
+    public serverURL = process.env.BACKEND_URL || 'http://localhost:8000'
 }
